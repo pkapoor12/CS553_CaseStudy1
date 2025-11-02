efee1b4a38b798279fe073c8616225965ac4bc3d
Pujeet Kapoor <pujkap123@gmail.com>
2025-11-02 17:46:43 -0500

    Updated server address

--- app.py
+++ app.py
@@ -475,4 +475,4 @@ with gr.Blocks(css=fancy_css) as demo:
     chatbot.render()
 
 if __name__ == "__main__":
-    demo.launch(share=True)
+    demo.launch(server_name="0.0.0.0", server_port=7860, share=False)
